337a9ebca20d5e5bab92db528249a0aaae799011
vasily <vasily@ctrlstack.com>
2023-06-23 14:22:10 -0500

    test with python 3.11

--- Formula/promptops-cli.rb
+++ Formula/promptops-cli.rb
@@ -15,7 +15,7 @@ class PromptopsCli < Formula
   end
 
   depends_on "numpy@1.25"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   resource "levenshtein" do
     url "https://files.pythonhosted.org/packages/c4/04/9179c510aec74ab84f3e6378526365b9fbe6d2a0d031ea178877c3bd5451/Levenshtein-0.21.1.tar.gz"
